73d53a8b1d24e4471da18262840ac441add2efba
Aliaksandr Khudnitski <alex2khudnitski@gmail.com>
2020-03-22 02:37:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(Array.isArray(array) && array.length > 0){
+        let minValue = array[0];
+        array.forEach(elem => (elem < minValue) ? minValue = elem : minValue);
+        return minValue;
+    }
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(Array.isArray(array) && array.length > 0){
+        let maxValue = array[0];
+        array.forEach(elem => (elem > maxValue) ? maxValue = elem : maxValue);
+        return maxValue;
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(Array.isArray(array) && array.length > 0){
+        const counter = array.length;
+        let summary = array.reduce((accumulator, currentValue, currentIndex) => (accumulator + currentValue));
+        return (summary/counter);
+    }
+    return 0;
 }
